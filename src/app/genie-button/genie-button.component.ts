10a5fb07ffbad53f00a90f116e55ad574bde575f
shaneGoose <shanegu53@hotmail.com>
2022-05-26 14:03:01 -0400

    4 files modified: interval appearance

--- src/app/genie-button/genie-button.component.ts
+++ src/app/genie-button/genie-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-genie-button',
@@ -6,9 +6,27 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   styleUrls: ['./genie-button.component.css'],
 })
 export class GenieButtonComponent implements OnInit, OnDestroy {
+  toAnimate = false;
+  isAnimating = false;
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    if (!this.isAnimating) {
+      console.log("Entered area");
+      this.toAnimate = true;
+      this.testAnimation();
+    }
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.toAnimate = false;
+  }
+
   // In px, sprite sheet is 3x4
   private readonly spriteSheet = { width: 1080, height: 1268 };
   private readonly sprite = { width: 360, height: 317 };
+  private readonly speed = 150;
   private readonly GENIE = [
     { y: 0, x: 0 },
     { y: 0, x: 1 },
@@ -32,37 +50,72 @@ export class GenieButtonComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.spriteContainer = document.getElementById('genie-sprite');
-
-    this.testAnimation();
+    this.position = GENIE_POS.HIDE.valueOf();
   }
 
   ngOnDestroy(): void {
-    if (this.animationInterval) clearInterval(this.animationInterval);
+    if (this.animationInterval) {
+      clearInterval(this.animationInterval);
+    }
   }
 
   private testAnimation() {
-    const speed = 100;
-    this.position = 0;
+    this.isAnimating = true;
 
     this.animationInterval = setInterval(() => {
+      console.log(`Position: ${this.position}`);
       const x = this.GENIE[this.position].x * this.sprite.width;
       const y = this.GENIE[this.position].y * this.sprite.height;
       this.spriteContainer.style.backgroundPosition = `-${x}px -${y}px`;
 
       this.position = (this.position + 1) % this.GENIE.length;
-    }, speed);
+
+      // Exit animation if left
+      if (!this.toAnimate && this.position === GENIE_POS.HIDE.valueOf() + 1) {
+        if (this.animationInterval) clearInterval(this.animationInterval);
+        this.isAnimating = false;
+      }
+      // TEST
+      else if (this.position === GENIE_POS.HIDE.valueOf() + 1) {
+        clearInterval(this.animationInterval);
+        setTimeout(() => {
+          this.testAnimation();
+        }, 3_000);
+      }
+    }, this.speed);
   }
+
+  // Intent is to have the Genie Rise and stay
+  private rise() {
+    // this.animationInterval = setInterval(() => {
+    //   const x = this.GENIE[this.position].x * this.sprite.width;
+    //   const y = this.GENIE[this.position].y * this.sprite.height;
+    //   this.spriteContainer.style.backgroundPosition = `-${x}px -${y}px`;
+    //   this.position = (this.position + 1) % this.GENIE.length;
+
+    //   if ((this.position = GENIE_POS.RAISE_BROWS.valueOf())) {
+    //     this.animationInterval = clearInterval(this.animationInterval);
+    //     --this.position;
+    //   }
+    // }, this.speed);
+  }
+
+  // Intent is to have the Genie shrug eyebrows while user is hovering
+  private shrugEyes() {}
+
+  // Intent is to have the Genie lower when the user's mouse leaves the area
+  private lower() {}
 }
 
 enum GENIE_POS {
+  HIDE,
   UP0,
   UP1,
   UP2,
   UP3,
   UP4,
-  UP5,
   PEAK,
-  RAISE_BROW,
+  RAISE_BROWS,
   DOWN0,
   DOWN1,
   DOWN2,
